6fd7fa6bfac0936811636da649a150090cc28c3a
sneha-tiwari06 <tiwarisneha491@gmail.com>
2024-07-05 17:21:00 +0530

    first

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 const corsOptions = {
-  origin: "https://live-server-bh9y.onrender.com",
+  origin: "https://ecis.in",
   credentials: true,
 };
 
