42adab54dadf8bd8e8aefcd75ee0ef4b613d722e
sneha-tiwari06 <tiwarisneha491@gmail.com>
2024-07-09 10:50:48 +0530

    testing folder

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(cookieParser());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./admin-smartex/upload");
+    cb(null, "/upload");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + file.originalname);
