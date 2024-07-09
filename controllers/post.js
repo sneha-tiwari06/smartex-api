e166ad3c378578a2019c16eab066a96e2d95cbe1
sneha-tiwari06 <tiwarisneha491@gmail.com>
2024-07-09 17:07:37 +0530

    msg

--- controllers/post.js
+++ controllers/post.js
@@ -17,7 +17,7 @@ export const getPosts = (req, res) => {
 
 export const getPost = (req, res) => {
   const q =
-    "SELECT p.id, `username`, `title`, `desc`, `img, `cat`,`date`, `meta_title`, `meta_keywords`, `meta_desc`, `blog_by`, `blog_date`, `blog_image_title` WHERE id =?";
+    "SELECT p.id, `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`,`date`, `meta_title`, `meta_keywords`, `meta_desc`, `blog_by`, `blog_date`, `blog_image_title` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ? ";
 
   db.query(q, [req.params.id], (err, data) => {
     if (err) return res.status(500).json(err);
@@ -30,7 +30,7 @@ export const addPost = async (req, res) => {
   try {
 
     const q =
-      "INSERT INTO posts(`title`, `desc`, `img`, `cat`, `date`, `meta_title`, `meta_keywords`, `meta_desc`, `blog_by`, `blog_date`, `blog_image_title`) VALUES (?)";
+      "INSERT INTO posts(`title`, `desc`, `img`, `cat`, `date`, `uid`, `meta_title`, `meta_keywords`, `meta_desc`, `blog_by`, `blog_date`, `blog_image_title`) VALUES (?)";
 
     const values = [
       req.body.title,
@@ -38,6 +38,7 @@ export const addPost = async (req, res) => {
       req.body.img,
       req.body.cat,
       req.body.date,
+      userInfo.id,
       req.body.meta_title,
       req.body.meta_keywords,
       req.body.meta_desc,
@@ -60,7 +61,7 @@ export const addPost = async (req, res) => {
 export const deletePost = (req, res) => {
 
     const postId = req.params.id;
-    const q = "DELETE FROM posts WHERE `id` = ?";
+    const q = "DELETE FROM posts WHERE `id` = ? AND `uid` = ?";
 
     db.query(q, [postId], (err, data) => {
       if (err) return res.status(403).json("You can delete only your post!");
@@ -91,13 +92,13 @@ export const updatePost = (req, res) => {
       let values;
 
       if (active === false) {
-        q = "UPDATE posts SET active = ? WHERE id = ?";
+        q = "UPDATE posts SET active = ? WHERE id = ? AND uid = ?";
         values = [false, postId];
       } else if (active === true) {
-        q = "UPDATE posts SET active = ? WHERE id = ? ";
+        q = "UPDATE posts SET active = ? WHERE id = ? AND uid = ?";
         values = [true, postId];
       } else {
-        q = "UPDATE posts SET `title`=?, `desc`=?, `img`=?, `cat`=?, `meta_title`=?, `meta_keywords`=?, `meta_desc`=?, `blog_by`=?, `blog_date`=?, `blog_image_title`=?, `active`=? WHERE `id` = ? ";
+        q = "UPDATE posts SET `title`=?, `desc`=?, `img`=?, `cat`=?, `meta_title`=?, `meta_keywords`=?, `meta_desc`=?, `blog_by`=?, `blog_date`=?, `blog_image_title`=?, `active`=? WHERE `id` = ? AND `uid` = ?";
         values = [
           title,
           desc,
