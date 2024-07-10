3140072fae9f765ca0b1c305558d9fff8df16fc1
sneha-tiwari06 <tiwarisneha491@gmail.com>
2024-07-10 13:17:40 +0530

    main

--- controllers/post.js
+++ controllers/post.js
@@ -17,7 +17,7 @@ export const getPosts = (req, res) => {
 
 export const getPost = (req, res) => {
   const q =
-    "SELECT id, `title`, `desc`, `img`, `cat`,`date`, `meta_title`, `meta_keywords`, `meta_desc`, `blog_by`, `blog_date`, `blog_image_title` FROM posts id = ? ";
+    "SELECT id, `title`, `desc`, `img`, `cat`,`date`, `meta_title`, `meta_keywords`, `meta_desc`, `blog_by`, `blog_date`, `blog_image_title` FROM posts WHERE id = ? ";
 
   db.query(q, [req.params.id], (err, data) => {
     if (err) return res.status(500).json(err);
